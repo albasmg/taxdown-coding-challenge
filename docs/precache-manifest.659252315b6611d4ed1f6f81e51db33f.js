0c1b22a0affb096be61df4c16bce8c47e1dd5dd8
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-09-16 14:10:03 +0200

    deploy app

---
+++ docs/precache-manifest.659252315b6611d4ed1f6f81e51db33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a463f01dc326c109b563597a2891ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "944dc7f236a2ee341e8c",
+    "url": "./static/css/main.d2d64dfe.chunk.css"
+  },
+  {
+    "revision": "6d7bd32c2e22b4f98992",
+    "url": "./static/js/2.0efa0244.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.0efa0244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944dc7f236a2ee341e8c",
+    "url": "./static/js/main.8676e780.chunk.js"
+  },
+  {
+    "revision": "f4060f919653bf35ca32",
+    "url": "./static/js/runtime-main.bd34ba93.js"
+  },
+  {
+    "revision": "785c6b41810eed87d95e2633b4910da4",
+    "url": "./static/media/logo.785c6b41.svg"
+  }
+]);
